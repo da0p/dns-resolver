c290770bc3638341e2e97ab3ec574e4c16fa0155
Phuong Dao <phuong018@gmail.com>
2024-08-04 22:58:19 +0200

    Use clap to parse command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+use clap::Parser;
+
 pub mod client;
 
+#[derive(Parser, Debug)]
+struct Options {
+    /// Host name that is needed to resolve
+    host: String,
+    /// DNS server
+    dns_server: String,
+}
+
 fn main() {
+    let options = Options::parse();
     let dns_client = client::DnsClient::new();
-    dns_client.ask("dns.google.com", "8.8.8.8");
+    dns_client.ask(&options.host, &options.dns_server);
 }
